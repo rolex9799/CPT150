d3942eb582beb163749607f31bb08d8aea4350d7
rolex9799 <rolandsrepetto3@inbox.lv>
2022-11-20 12:33:31 +0100

    JS completed!

--- Final project/script.js
+++ Final project/script.js
@@ -1,2 +1,51 @@
+const playerText = document.querySelector(".playerScore");
 
+const computerText = document.querySelector(".computerScore");
 
+const resultText = document.querySelector(".result");
+
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+        computer = "Rock";
+        break;
+        case 2:
+        computer = "Paper";
+        break;
+        case 3:
+        computer = "Scissors";
+        break;
+    }
+}
+
+function checkWinner(){
+    if (player == computer){
+        return "Draw!";
+    }
+    else if (computer == "Rock"){
+        return (player == "Paper")? "You Win!" : "You Lose!"
+    }
+    else if (computer == "Paper"){
+        return (player == "Scissor")? "You Win!" : "You Lose!"
+    }
+    else if (computer == "Scissors"){
+        return (player == "Rock")? "You Win!" : "You Lose!"
+    }
+}
